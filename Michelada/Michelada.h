c4f07093ef5ff4d4bc6473d9d439819956077e77
Satyendra Singh <satyendra.singh@mykaarma.com>
2020-02-03 16:58:05 -0800

    Initial project setup

---
+++ Michelada/Michelada.h
@@ -0,0 +1,19 @@
+//
+//  Michelada.h
+//  Michelada
+//
+//  Created by Satyendra Singh on 03/02/20.
+//  Copyright © 2020 myKaarma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Michelada.
+FOUNDATION_EXPORT double MicheladaVersionNumber;
+
+//! Project version string for Michelada.
+FOUNDATION_EXPORT const unsigned char MicheladaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Michelada/PublicHeader.h>
+
+
